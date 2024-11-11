968cda3cc7013f404e84dafdda0cf0f12e9abd92
jayson <sondanghuynh@gmail.com>
2024-11-11 23:34:51 +0700

    Fix Entity v2

--- src/main/java/com/adidark/entity/PaymentEntity.java
+++ src/main/java/com/adidark/entity/PaymentEntity.java
@@ -7,11 +7,9 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 @EqualsAndHashCode(callSuper = true)
 @Data

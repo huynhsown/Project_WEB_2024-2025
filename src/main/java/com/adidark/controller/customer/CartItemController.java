486eab08f256a672fbd06ffdaf87c9ebc25921b7
HoangPhuc <phucnguyeho@gmail.com>
2024-12-02 19:16:18 +0700

    phuc_dev: have at least 1 select before click buy

--- src/main/java/com/adidark/controller/customer/CartItemController.java
+++ src/main/java/com/adidark/controller/customer/CartItemController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -123,8 +124,8 @@ public class CartItemController {
         cartService.save(cartEntity);
     }
 
-    @PostMapping("/delete")
-    public String deleteCartItem(
+    @GetMapping("/delete")
+    public String deleteCartItem (
         @RequestParam Long cartItemId,
         Model model
     ) {
